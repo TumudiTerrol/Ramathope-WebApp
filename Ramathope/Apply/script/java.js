653dc741b7a944a24006ea79ea0e5bcf3a1f809e
TumudiTerrol <163354790+TumudiTerrol@users.noreply.github.com>
2024-03-15 02:05:55 +0200

    Add files via upload

---
+++ Ramathope/Apply/script/java.js
@@ -0,0 +1,13 @@
+// ==========================================
+let popup = document.getElementById("popup");
+// ====== OPENING POPUP MESSAGE =======
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+// ===================================
+
+//======= CLOSING POPUM MESSAGE =======
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
+//======================================
---
+++ Ramathope/JS/index.js
@@ -0,0 +1,15 @@
+// ==========================================
+let popup = document.getElementById("popup");
+// ====== OPENING POPUP MESSAGE =======
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+// ===================================
+
+//======= CLOSING POPUM MESSAGE =======
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
+//======================================
+
+
